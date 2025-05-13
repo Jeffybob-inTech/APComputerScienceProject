9bc813de119eb537eb0c5e1b51a04fb56a26903a
Jeffybob-inTech <jcmercer0713@gmail.com>
2025-05-13 16:35:32 +0000

    creating the bones of the game, nothing major yet

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+let thisRunsContribution = 0;
+let multiplier = 1;
+let price = 10
+const kornsHeadBtn = document.getElementById("kornsHead");
+const personalContributionOfKernalsEl = document.getElementById("personalContributionOfKernalsEl");
+const groupKernalsEl = document.getElementById("groupKernalsEl");
+const marketOption = document.getElementById("marketOption")
+kornsHeadBtn.addEventListener("click", function(){
+    thisRunsContribution += multiplier;
+    personalContributionOfKernalsEl.textContent = thisRunsContribution;
+    groupKernalsEl.textContent = thisRunsContribution;
+})
 
+marketOption.addEventListener("click", function(){
+    if(thisRunsContribution < price){
+        return;
+    }
+    multiplier += 1;
+    thisRunsContribution -= price;
+    personalContributionOfKernalsEl.textContent = thisRunsContribution;
+    groupKernalsEl.textContent = thisRunsContribution;
+})
