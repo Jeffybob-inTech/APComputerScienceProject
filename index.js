02f6032b076da216d29776e9ca9bf7cdb3c922ba
Jeffybob-inTech <jcmercer0713@gmail.com>
2025-05-15 16:52:01 +0000

    adding new characters and price maniulation

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-let thisRunsContribution = 0;
-let multiplier = 1;
-let price = 10
+const characterSelectionCn = document.getElementById("characterSelectionCn");
 const kornsHeadBtn = document.getElementById("kornsHead");
 const personalContributionOfKernalsEl = document.getElementById("personalContributionOfKernalsEl");
 const groupKernalsEl = document.getElementById("groupKernalsEl");
-const marketOption = document.getElementById("marketOption")
+const marketOption = document.getElementById("marketOption");
+const characterSelectionButton  = document.getElementById("Icon");
+const marketOptionCostEl = document.getElementById("marketOptionCostEl")
+//character selection
+characterSelectionButton.addEventListener("click", function(){
+    characterSelectionCn.style.display = "none";
+})
+//playing the game
+let thisRunsContribution = 0;
+let multiplier = 1;
+let price = 10;
+let numberBought = 0;
 kornsHeadBtn.addEventListener("click", function(){
     thisRunsContribution += multiplier;
     personalContributionOfKernalsEl.textContent = thisRunsContribution;
@@ -16,7 +25,20 @@ marketOption.addEventListener("click", function(){
         return;
     }
     multiplier += 1;
+    
     thisRunsContribution -= price;
+    price *= 1.8 + numberBought;
+    numberBought++;
+    marketOptionCostEl.textContent = price;
     personalContributionOfKernalsEl.textContent = thisRunsContribution;
     groupKernalsEl.textContent = thisRunsContribution;
 })
+//market options
+/*
+ popkorn
+ small fire
+ popkorn popper
+ movie theater
+ popkorn factory
+ popkorn empire
+ */
